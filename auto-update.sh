fcd0aedabb2d5c274cc50d047e9702a2e7350e68
Giovanni Grieco <giovanni@grieco.dev>
2022-04-14 10:59:03 +0200

    Improve auto-update from upstream

--- auto-update.sh
+++ auto-update.sh
@@ -2,8 +2,16 @@
 
 set -e
 
+# Download new updates from upstream
+git checkout beta
 git pull public beta
+git push
+
+# Rebase wayland_support branch
+git checkout wayland_support
+git rebase beta
+git push --force
+
 flatpak remove --assumeyes com.visualstudio.code.insiders
 ./build.sh
 ./install.sh
-
